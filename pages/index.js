70e11a6507a968459b9f491388bf3fd9f22e0279
wes <107316719+wespistoy@users.noreply.github.com>
2022-06-15 12:06:25 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -57,7 +57,7 @@ const App = () => {
             href={TWITTER_LINK}
             target="_blank"
             rel="noreferrer"
-          >{`built on @${TWITTER_HANDLE}`}</a>
+          >{`@${TWITTER_HANDLE}`}</a>
            </a>
         </div>
       </div>
